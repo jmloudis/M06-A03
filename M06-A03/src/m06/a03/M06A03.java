774287c058982e5100d77f4e9aeb26b910f72cda
Jordan Loudis <16jloudis@gmail.com>
2020-10-13 15:44:52 -0400

    test 1

---
+++ M06-A03/src/m06/a03/M06A03.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m06.a03;
+
+/**
+ *
+ * @author jordan
+ */
+public class M06A03
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello GitHub");
+        // TODO code application logic here
+    }
+    
+}
